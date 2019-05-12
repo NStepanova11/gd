9a8650c108086d77f676461ada092f9d3ece804f
Nadezhda Stepanova <n.stepanova11@yandex,ru>
2019-05-12 18:32:41 +0300

    ...

---
+++ testingshapes/Assets/Scripts/SceneController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    public const int gridRows = 2; 
+    public const int gridCols = 4;
+    public const float offsetX = 2f;
+    public const float offsetY = 2.5f;
+
+    [SerializeField]
+    private ShapeScript shape; 
+    [SerializeField] private Sprite[] images;
+    void Start()
+    {
+        int id = 0;
+        int[,] main100pos = new int[,] { { 0, 0 }, { 0, 2 }, { -1, -1 }, { -1, 1 }, { 2, -2 } };
+        
+        for (int i=0; i<5; i++)
+        {
+            ShapeScript cloneShape;
+            cloneShape = Instantiate(shape) as ShapeScript;
+            cloneShape.tag = "MainShape";
+            cloneShape.SetShape(images[id]);
+            cloneShape.transform.position = new Vector3(main100pos[i,0], main100pos[i,1], 0);
+        }
+                
+        id++;
+        int[,] sub110Pos = new int[,] { { 0, -2 }, { 2, 0 }, { -2, 2 }, { -2, -2 } };
+        for (int i = 0; i < 4; i++)
+        {
+            ShapeScript cloneShape;
+            cloneShape = Instantiate(shape) as ShapeScript;
+            cloneShape.tag = "SubShape";
+            cloneShape.SetShape(images[id]);
+            cloneShape.transform.position = new Vector3(sub110Pos[i, 0], sub110Pos[i, 1], 0);
+        }
+
+        id++;
+        int[,] sub120Pos = new int[,] { { -2, 0 }, { 1, -1 }, { 1, 1 }, { 2, 2 } };
+        for (int i = 0; i < 4; i++)
+        {
+            ShapeScript cloneShape;
+            cloneShape = Instantiate(shape) as ShapeScript;
+            cloneShape.tag = "SubShape";
+            cloneShape.SetShape(images[id]);
+            cloneShape.transform.position = new Vector3(sub120Pos[i, 0], sub120Pos[i, 1], 0);
+        }
+    }
+}
---
+++ testingshapes/Assets/Scripts/ShapeScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeScript : MonoBehaviour
+{
+    private Renderer renderrer;
+    [SerializeField]
+    private SceneController controller;
+   
+    private bool mainShapeIsFind = false;
+    private GameObject[] goodShapes, badShapes, allShapes;
+
+    public void OnMouseDown()
+    {
+        renderrer = GetComponent<SpriteRenderer>();
+        Debug.Log("go tag = " + renderrer.tag);
+        if (renderrer.tag=="MainShape")
+        {
+            for (int i = 0; i < goodShapes.Length; i++)
+            {
+                Renderer rend = goodShapes[i].GetComponent<SpriteRenderer>();
+                rend.material.color = Color.green;
+            }
+            Debug.Log("you win");
+            mainShapeIsFind = true;
+        }
+        else
+        {
+            for (int i = 0; i < goodShapes.Length; i++)
+            {
+                Renderer rend = goodShapes[i].GetComponent<SpriteRenderer>();
+                rend.material.color = Color.red;
+            }
+            for (int i = 0; i < badShapes.Length; i++)
+            {
+                Renderer rend = badShapes[i].GetComponent<SpriteRenderer>();
+                rend.material.color = Color.red;
+            }
+            Debug.Log("you lose");
+        }
+    }
+
+    public void SetShape(Sprite image)
+    {
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+
+    void Start()
+    {
+        goodShapes = GameObject.FindGameObjectsWithTag("MainShape");
+        badShapes = GameObject.FindGameObjectsWithTag("SubShape");
+    }
+}
