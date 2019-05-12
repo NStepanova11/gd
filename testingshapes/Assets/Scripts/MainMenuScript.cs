de6fa2b51ad82face09ee7010ffc5fcdd4f255d2
Nadezhda Stepanova <n.stepanova11@yandex,ru>
2019-05-12 21:22:25 +0300

    ...

---
+++ testingshapes/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void PlayButtonPressed()
+    {
+        SceneManager.LoadScene("Level_1_Scene");
+    }
+
+	public void HelpButtonPressed()
+	{
+		SceneManager.LoadScene("Help_Scene");
+	}
+    public void ExitButtonPressed()
+    {
+        Application.Quit();
+        Debug.Log("Exit pressed!");
+    }
+}
